44839f4e0bf59a167e610633d1d2c43069fde910
kalvin <36674507+kalvincui@users.noreply.github.com>
2022-12-29 15:53:23 -0500

    Update Queue.cpp

--- Queues/Queue/Queue.cpp
+++ Queues/Queue/Queue.cpp
@@ -7,15 +7,15 @@ int main () {
     // queue <datatype> name;
     queue <int> intqueue;
     intqueue.push(1);
-    intqueue.push(2);
     intqueue.push(3);
+    intqueue.push(2);
     while (!intqueue.empty()){
         cout << intqueue.back() << '\n';
         intqueue.pop();
     }
     intqueue.push(1);
-    intqueue.push(2);
     intqueue.push(3);
+    intqueue.push(2);
     while (!intqueue.empty()){
         cout << intqueue.front() << '\n';
         intqueue.pop();
@@ -23,10 +23,10 @@ int main () {
 }
 
 /* Output
-3
-3
-3
-1
 2
+2
+2
+1
 3
+2
 */
